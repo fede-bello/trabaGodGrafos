4f1c87f8037e3269f909be2c1ee7a71a2f72ba90
gonzachiar <gonza@Gonzalos-MacBook-Air.local>
2024-02-03 16:18:37 -0300

    added labels

--- research/20240203_gch_initial_research.py
+++ research/20240203_gch_initial_research.py
@@ -1,1 +1,16 @@
 # %%
+import pandas as pd
+import start_research
+
+# %%
+df_train = pd.read_csv("data/TELCO_data_train.csv")
+df_train_labels = pd.read_csv("data/TELCO_labels_train.csv")
+df_val = pd.read_csv("data/TELCO_data_val.csv")
+df_val_labels = pd.read_csv("data/TELCO_labels_val.csv")
+df_test = pd.read_csv("data/TELCO_data_test.csv")
+df_test_labels = pd.read_csv("data/TELCO_labels_test.csv")
+# %%
+df_train
+# %%
+df_test
+# %%
